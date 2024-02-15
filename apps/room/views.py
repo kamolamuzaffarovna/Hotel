f273ef0b603ab274233079ebb55534e1be19ac61
Kamola Kamola <kamolaxaytboyeva0509@gmail.com>
2024-02-15 23:54:27 +0500

    initial

--- apps/room/views.py
+++ apps/room/views.py
@@ -6,6 +6,19 @@ from .models import Room, FooterImage, Information, Service
 class RoomListView(TemplateView):
     template_name = 'room/room_list.html'
 
+    def get_list(self):
+        return Room.objects.all()
+
+    def get_data(self):
+        return Information.objects.all()
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['list'] = self.get_list()
+        ctx['data'] = self.get_data()
+
+        return ctx
+
 
 class RoomDetailView(TemplateView):
     template_name = 'room/room_detail.html'
