574814d03dbbfcc2ac45159e12a8fc30f4345ff8
Kamola Kamola <kamolaxaytboyeva0509@gmail.com>
2024-03-04 06:11:34 +0500

    initial

--- apps/main/forms.py
+++ apps/main/forms.py
@@ -2,47 +2,28 @@ from django import forms
 from apps.room.models import Booking
 
 
+from django import forms
+from apps.room.models import Booking
+
+
 class RoomBronForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['check_in', 'check_out', 'room', 'adults', 'children', 'price']
-    #
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['check_in'].widget.attrs.update({
-    #         'type': 'date',
-    #         'class': 'form-control',
-    #         'id': 'checkIn',
-    #         'name': 'check_in',
-    #         'placeholder': 'Check In'
-    #     })
-    #     self.fields['check_out'].widget.attrs.update({
-    #         'type': 'date',
-    #         'class': 'form-control',
-    #         'id': 'checkOut',
-    #         'name': 'check_out',
-    #         'placeholder': 'Check Out'
-    #     })
-    #     self.fields['adults'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'id': 'adults',
-    #         'name': 'adults',
-    #     })
-    #     self.fields['room'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'id': 'room',
-    #         'name': 'room',
-    #     })
-    #     self.fields['children'].widget.attrs.update({
-    #         'class': 'form-control',
-    #         'id': 'children',
-    #         'name': 'children',
-    #     })
-        # self.fields['price'].widget.attrs.update({
-        #     'type': 'text',
-        #     'class': 'input-small form-control',
-        #     'id': 'checkInDate',
-        #     'name': 'checkInDate',
-        #     'placeholder': 'Check In'
-        # })
 
+    def init(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['check_in'].widget.attrs.update({
+            'type': 'date',
+            'class': 'form-control',
+            'id': 'checkIn',
+            'name': 'check_in',
+            'placeholder': 'Check In'
+        })
+        self.fields['check_out'].widget.attrs.update({
+            'type': 'date',
+            'class': 'form-control',
+            'id': 'checkOut',
+            'name': 'check_out',
+            'placeholder': 'Check Out'
+        })
--- apps/room/views.py
+++ apps/room/views.py
@@ -18,16 +18,15 @@ class RoomListView(View):
         check_out = request.GET.get('check_out')
         adults = request.GET.get('adults')
         children = request.GET.get('children')
+        paginator = Paginator(rooms, 1)
+        page = request.GET.get('page')
+        page_obj = paginator.get_page(page)
         if check_in and check_out:
-            rooms = rooms.filter(
-                ~Q(rooms_booking__check_in__lte=check_out) | ~Q(rooms_booking__check_out__gte=check_in))
+            rooms = rooms.filter(~Q(booking__check_in__lte=check_out) | ~Q(booking__check_out__gte=check_in))
         if adults or children:
             adults = int(adults)
             children = int(children)
             rooms = rooms.filter(Q(children=children) or Q(adults=adults))
-        paginator = Paginator(rooms, 1)
-        page = request.GET.get('page')
-        page_obj = paginator.get_page(page)
 
         ctx = {
             'rooms': rooms,
@@ -44,43 +43,6 @@ class RoomDetailView(View):
     def get(self, request, *args, **kwargs):
         objects = get_object_or_404(Room, slug=kwargs['slug'])
         form = RoomBronForm()
-        ctx = {
-
-        }
-        if request.method == 'POST':
-            check_in = request.POST.get('check_in')
-            check_out = request.POST.get('check_out')
-            if check_in is not None:
-                check_in = '20' + '-'.join(check_in.split('/')[::-1])
-            if check_out is not None:
-                check_out = '20' + '-'.join(check_out.split('/')[::-1])
-            date = {
-                'check_in': check_in,
-                'check_out': check_out
-            }
-            check_in_date = datetime.strptime(check_in, '%Y-%m-%d')
-            check_out_date = datetime.strptime(check_out, '%Y-%m-%d')
-            farq = abs((check_out_date - check_in_date).days)
-            ctx['farq'] = farq
-            form = RoomBronForm(date)
-            if form.is_valid():
-                if objects.is_open_for_booking and farq > 0:
-                    obj = form.save(commit=False)
-                    obj.objects = objects
-                    obj.author_id = request.user.id
-                    form.save()
-                    objects.is_open_for_booking = False
-                    objects.save()
-                    messages.success(request,
-                                     f'You booked the room for {farq} days and the total price ${farq * objects.price}')
-                    return redirect('.')
-                else:
-                    messages.success(request, 'Sorry, the room is already booked!!!')
-            else:
-                messages.success(request, 'the room is booking!!!')
-
-            return render(request, self.template_name, ctx)
-
         image = FooterImage.objects.all()
         data = Information.objects.all()
         services = Service.objects.all()
@@ -98,42 +60,35 @@ class RoomDetailView(View):
 
         return render(request, self.template_name, ctx)
 
-    # def post(self, request, *args, **kwargs):
-    #     objects = get_object_or_404(Room, slug=kwargs['slug'])
-    #     form = RoomBronForm()
-    #     ctx = {
-    #         'objects': objects,
-    #         'form': form
-    #     }
-    #     check_in = request.POST.get('check_in')
-    #     check_out = request.POST.get('check_out')
-    #     if check_in is not None:
-    #         check_in = '20' + '-'.join(check_in.split('/')[::-1])
-    #     if check_out is not None:
-    #         check_out = '20' + '-'.join(check_out.split('/')[::-1])
-    #     date = {
-    #         'check_in': check_in,
-    #         'check_out': check_out
-    #     }
-    #     check_in_date = datetime.strptime(check_in, '%Y-%m-%d')
-    #     check_out_date = datetime.strptime(check_out, '%Y-%m-%d')
-    #     farq = abs((check_out_date - check_in_date).days)
-    #     ctx['farq'] = farq
-    #     form = RoomBronForm(date)
-    #     if form.is_valid():
-    #         if objects.is_open_for_booking and farq>0:
-    #             obj = form.save(commit=False)
-    #             obj.objects = objects
-    #             obj.author_id = request.user.id
-    #             form.save()
-    #             objects.is_open_for_booking = False
-    #             objects.save()
-    #             messages.success(request,
-    #                              f'You booked the room for {farq} days and the total price ${farq * objects.price}')
-    #             return redirect('.')
-    #         else:
-    #             messages.success(request, 'Sorry, the room is already booked!!!')
-    #     else:
-    #         messages.success(request, 'the room is booking!!!')
-    #
-    #     return render(request, self.template_name, ctx)
+    def post(self, request, *args, **kwargs):
+        objects = get_object_or_404(Room, slug=kwargs['slug'])
+        form = RoomBronForm()
+        ctx = {
+            'objects': objects,
+            'form': form
+        }
+        check_in = '20' + '-'.join(request.POST['check_in'].split('/')[::-1])
+        check_out = '20' + '-'.join(request.POST['check_out'].split('/')[::-1])
+        date = {
+            'check_in': check_in,
+            'check_out': check_out,
+        }
+        print(check_in, check_out)
+        farq = abs((datetime.strptime(check_out, '%Y-%m-%d') - datetime.strptime(check_in, '%Y-%m-%d')).days)
+        ctx['farq'] = farq
+        form = RoomBronForm(date)
+        print(form.errors)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.objects = objects
+            obj.author_id = request.user.id
+            form.save()
+            objects.is_open_for_booking = False
+            objects.save()
+            messages.success(request,
+                             f'You booked the room for {farq} days and the total price ${farq * objects.price}')
+            return redirect('.')
+        else:
+            messages.success(request, 'the room is booking!!!')
+
+        return render(request, self.template_name, ctx)
